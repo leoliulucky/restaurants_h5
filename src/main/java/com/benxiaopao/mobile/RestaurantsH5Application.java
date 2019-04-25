2eb52b9f0083be5056903998a8a34cdf95576b72
leoliulucky <591525354@qq.com>
2019-04-25 08:19:18 +0800

    init commit

---
+++ src/main/java/com/benxiaopao/mobile/RestaurantsH5Application.java
@@ -0,0 +1,13 @@
+package com.benxiaopao.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsH5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantsH5Application.class, args);
+    }
+
+}
